dda111b3151cc26713faf6363a4c813b6e833ff6
Matt Donders <mdonders@gmail.com>
2018-11-30 11:37:47 -0500

    Added some error checking, line update date and moved POST method around.

--- lines.py
+++ lines.py
@@ -1,7 +1,8 @@
 import json
 import os
+from datetime import datetime
 
-from flask import Flask, flash, render_template, request
+from flask import Flask, flash, render_template, request, redirect, url_for
 from wtforms import Form, StringField, SubmitField, TextAreaField, validators
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
@@ -9,7 +10,7 @@ PROJECT_ROOT_1LVL = os.path.dirname(PROJECT_ROOT)
 LINES_FILE = os.path.join(PROJECT_ROOT_1LVL, 'nhl-twitter-bot', 'localdata', 'lines-override.json')
 
 # Flask Application Config
-#DEBUG = True
+# DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '6ac69d35549cb232a7946da014f03a68030991afc7a7f29b'
@@ -35,9 +36,26 @@ class LinesForm(Form):
     RD3 = StringField('RD3', [validators.DataRequired()], render_kw={"placeholder": "Right Defense #3"})
 
 def readLinesFile():
-    with open(LINES_FILE) as f:
-        lines = json.load(f)
-    return lines
+    try:
+        with open(LINES_FILE) as f:
+            lines = json.load(f)
+    except FileNotFoundError:
+        # 'Touch' the File
+        with open(LINES_FILE, 'a'):
+            os.utime(LINES_FILE, None)
+
+        # Return an empty dictionary
+        lines = {'1C': '', '1LW': '', '1RW': '',
+                 '2C': '', '2LW': '', '2RW': '',
+                 '3C': '', '3LW': '', '3RW': '',
+                 '4C': '', '4LW': '', '4RW': '',
+                 '1LD': '', '1RD': '',
+                 '2LD': '', '2RD': '',
+                 '3LD': '', '3RD': ''}
+
+    filetime = os.path.getmtime(LINES_FILE)
+
+    return (lines, filetime)
 
 
 def writeLinesFile(lines):
@@ -45,15 +63,25 @@ def writeLinesFile(lines):
         json.dump(lines, f)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def lines_form():
     form = LinesForm(request.form)
 
-    lines = readLinesFile()
+    lines, filetime = readLinesFile()
     print(f'Lines from File: {lines}')
 
-    if request.method == 'POST':
+    # Convert Filetime to String Format
+    # filetime = datetime.fromtimestamp(filetime).strftime('%Y-%m-%d %H:%M:%S')
+    filetime = datetime.fromtimestamp(filetime).strftime('%m-%d-%Y %H:%M%p')
 
+    return render_template('lines.html', form=form, lines=lines, filetime=filetime)
+
+
+@app.route("/updatelines", methods=['POST'])
+def update_lines():
+    form = LinesForm(request.form)
+
+    if request.method == 'POST':
         confirmed_lines = dict()
         for fieldname, value in form.data.items():
             # Convert fieldname from <POS><LINE> to <LINE><POS>
@@ -71,7 +99,7 @@ def lines_form():
         else:
             flash('All the form fields are required.', 'error')
 
-    return render_template('lines.html', form=form, lines=lines)
+    return redirect(url_for('lines_form'))
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
